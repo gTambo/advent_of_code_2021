5ac164c35ba1c9cd53e352ca20ed4f6c3a601c35
Greg Tambornino <gregtambornino@gmail.com>
2021-12-05 15:24:26 -0600

    cleaning up code

--- day1.js
+++ day1.js
@@ -1,19 +1,14 @@
-
 import puzzleInput from './day1Input.js';
 
-
 function countIncrease(arr) {    
+    
     // initialize counter variable
     let depthIncrease = 0;
     for (let i = 0; i <= arr.length; i++) {
         if (arr[i + 1] > arr[i]) {
             depthIncrease = depthIncrease += 1;
-            // console.log(arr[i], `increase at i =`, i);
-            
         } else {
             // do nothing
-            // console.log(arr[i], 'no increase at i =', i);
-            
         }
     }
     return depthIncrease;
@@ -34,26 +29,18 @@ const sampleArr = [
     270
 ];
 
-const otherArr = [
-    1230, 34934, 29, 239082, 483, 38, 8389, 939, 940, 955, 30, 35
-];
-
+// testing countIncrease function with sample array
 console.log('depth increase for other arr', countIncrease(sampleArr));
 
-// console.log('Puzzle Input', puzzleInput);
-
+// set new array to add integers from puzzleInput array
 const puzzleInputIntegersArr = [];
 
+// use parseInt to change strings of integers to plain integers and push to new array
 for (let i = 0; i < puzzleInput.length; i++) {
     puzzleInputIntegersArr.push(parseInt(puzzleInput[i]));
 } 
 
-// console.log('Puzzle Input integers', puzzleInputIntegersArr);
-
-// console.log('puzzle input array', puzzleInput);
-// console.log('last item of puzzleInputIntegersrr', puzzleInputIntegersArr[puzzleInputIntegersArr.length - 1]);
-
+// something about the array of strings isn't comparing correctly...
 console.log('depth increase for puzzle input', countIncrease(puzzleInput));
 
 console.log('depth increase for puzzle input', countIncrease(puzzleInputIntegersArr));
-// console.log('first item of puzzleInputCopy array ', puzzleInputArr[0]);
