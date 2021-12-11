971fbad8e5f2ff3b1c22c4673cebdfc2ad90ffcf
Greg Tambornino <gregtambornino@gmail.com>
2021-12-11 00:07:13 -0600

    first solution attempt

--- day8.js
+++ day8.js
@@ -3,10 +3,28 @@ import fs from 'fs';
 fs.readFile('inputD8.txt', function(err, data) {
     if(err) throw err;
 
-    const input = data.toString().replace(/\r\n/g,'\n').split('\n');
+    const input = data.toString().replace(/\r\n/g,'\n').split(' ');
 
-    for (let i of input) {
-        console.log(i);
+    let count1470 = 0;
+    for (let i =0; i < input.length; i++) {
+        // console.log(i);
+        
+        if (input[i] === '|') {
+            // console.log(input[i]);
+            for (let value = 1; value <= 4; value++) {
+                if (input[i + value].length === 2 || 
+                    input[i + value].length === 3 || 
+                    input[i + value].length === 4 || 
+                    input[i + value].length === 7) {
+                        console.log(input[i + value]);
+                        count1470++;
+                    }
+            }
+        }
+        
     }
+    console.log(count1470);
+    // console.log(input.length);
+    // console.log(input.at(52));
 
 });
