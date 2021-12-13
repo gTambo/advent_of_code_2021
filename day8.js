94f6b19382583e521822dcd83c433edec1cd7fc0
Greg Tambornino <gregtambornino@gmail.com>
2021-12-13 10:39:10 -0600

    using .split to correctly rreturn input

--- day8.js
+++ day8.js
@@ -1,23 +1,26 @@
+import { count } from 'console';
 import fs from 'fs';
 
 fs.readFile('inputD8.txt', function(err, data) {
     if(err) throw err;
 
-    const input = data.toString().replace(/\r\n/g,'\n').split(' ');
-
+    const input = data.toString().replace(/\r\n/g,'\n').split(/\s+/);
+    
     let count1470 = 0;
     for (let i =0; i < input.length; i++) {
-        // console.log(i);
+        // console.log(input[i],',');
         
         if (input[i] === '|') {
-            // console.log(input[i]);
-            for (let value = 1; value <= 4; value++) {
+            console.log('Current value', input[i], 'at input', i);
+            for (let value = 1; value < 5; value++) {
+                console.log('Next value', input[i + value]);
+
                 if (input[i + value].length === 2 || 
                     input[i + value].length === 3 || 
                     input[i + value].length === 4 || 
                     input[i + value].length === 7) {
-                        console.log(input[i + value]);
                         count1470++;
+                        console.log('count', count1470);
                     }
             }
         }
