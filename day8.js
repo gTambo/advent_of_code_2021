77618e6a6759d983947d6fb0bef1e16dd74be3bc
Greg Tambornino <gregtambornino@gmail.com>
2021-12-14 19:19:21 -0600

    thinking through positions

--- day8.js
+++ day8.js
@@ -1,8 +1,30 @@
-import { count } from 'console';
 import fs from 'fs';
 
 fs.readFile('inputD8.txt', function(err, data) {
     if(err) throw err;
+
+    // 7 possible positions
+    let p0; // top
+    let p1; // upper left 
+    let p2; // upper right
+    let p3; // center
+    let p4; // lower left
+    let p5; // lower right
+    let p6; // bottom
+
+    // unique signal patterns
+    // 1 => input[i].length == 2;
+    // 4 => input[i].length == 4;
+    // 7 => input[i].input == 3;
+    // 8 => input[i].length == 7;
+
+    // non-unique signal patterns
+    // '0' .length == 6
+    // '2' .length == 5
+    // '3' .length == 5
+    // '5' .length == 5
+    // '6' .length == 6
+    // '9' .length == 6
 
     const input = data.toString().replace(/\r\n/g,'\n').split(/\s+/);
     
