fc410bea423372377d781485e14c945502868647
Greg Tambornino <gregtambornino@gmail.com>
2021-12-15 00:13:49 -0600

    iterating through input by row

--- day8pt2.js
+++ day8pt2.js
@@ -81,31 +81,6 @@ fs.readFile('inputD8.txt', function(err, data) {
             
         }
     }
-    // for (let item of first) {
-        // console.log(item);
-    // }
-
-    let count1470 = 0;
-    for (let i =0; i < input.length; i++) {
-        // console.log(input[i],',');
-        
-        if (input[i] === '|') {
-            // console.log('Current value', input[i], 'at input', i);
-            for (let value = 1; value < 5; value++) {
-                // console.log('Next value', input[i + value]);
-
-                if (input[i + value].length === 2 || 
-                    input[i + value].length === 3 || 
-                    input[i + value].length === 4 || 
-                    input[i + value].length === 7) {
-                        count1470++;
-                        // console.log('count', count1470);
-                    }
-            }
-        }
-        
-    }
-    console.log(count1470);
     console.log(input.length);
     // console.log(input.at(52));
 
